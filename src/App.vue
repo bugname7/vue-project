148d784b127ac7ac4ce3ff9d03c25663961c0a05
bugname7 <valisher2106@gmail.com>
2025-04-28 22:11:15 +0500

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <template>
-  <div>
-    <button
-      @click="toggleLogin"
-      class="bg-black text-red-700 w-1/2 p-4 cursor-pointer"
-    >
-      {{ loggedIn ? 'Chiqish' : 'Kirish' }}
-    </button>
-  </div>
+  <div></div>
 </template>
-
 <script>
 export default {
-  data () {
+  data() {
     return {
-      loggedIn: false
+    
     }
   },
-  methods: {
-    toggleLogin () {
-      this.loggedIn = !this.loggedIn
-    }
-  }
 }
 </script>
