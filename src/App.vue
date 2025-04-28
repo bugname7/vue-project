4ed9b718559e7fc17f90b762b533854d0540e0f5
bugname7 <valisher2106@gmail.com>
2025-04-28 21:06:22 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Home :users="user" />
+  </div>
+</template>
+<script>
+import axios from 'axios'
+import Home from './components/home/Home.vue'
+
+export default {
+  components: {
+    Home
+  },
+  data () {
+    return {
+      user: []
+    }
+  },
+  mounted () {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        if (response.status == 200) {
+          this.user = response.data
+        } else {
+          console.log('xatolik bor qayerdadir', response.status)
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div v-for="(data, index) in users" :key="data.id">
+    <h2>{{ index + 1 }}-{{ data.email }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  props: ['users']
+}
+</script>
